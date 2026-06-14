2c8c7e797f5b564e6752b281983db42db972ac7c
Leif <leif.algo@pm.me>
2026-06-14 10:47:32 -0600

    Update hub.ts (#289)

--- site/src/lib/hub.ts
+++ site/src/lib/hub.ts
@@ -4,5 +4,5 @@
  *  - Marketing (incl. the languages gallery and blog) -> HUB_MARKETING
  *  - Docs and examples                                 -> HUB_DOCS
  */
-export const HUB_MARKETING = 'https://corvidlabs.github.io/corvidlabs-site/spec-sync/'
-export const HUB_DOCS = 'https://corvidlabs.github.io/corvidlabs-site/spec-sync/docs/'
+export const HUB_MARKETING = 'https://corvidlabs.xyz/spec-sync/'
+export const HUB_DOCS = 'https://corvidlabs.xyz/spec-sync/docs/'
